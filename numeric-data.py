749ece8f60ab606f9065228c25e2c4f4ac8981a4
Quayson97 <113901157+Quayson97@users.noreply.github.com>
2024-05-07 00:25:30 +0000

    Add files via upload
    Python codes from the AWS re/start labs

---
+++ numeric-data.py
@@ -0,0 +1,44 @@
+#Addtion
+print(2+2) 
+
+#Subtraction
+print(4-2)
+
+#Multiplication
+print(2*2)
+
+#Division
+print(4/2)
+
+#Data Types 
+print("Python has three numeric types: int, float, and complex")
+print(type("Python has three numeric types: int, float, and complex"))
+
+#Creating a Variable 
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Float Data type
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Complex Data type
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Bool Data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
