abcc6ba7be51076584171e6cd7909c562573033a
Quayson97 <113901157+Quayson97@users.noreply.github.com>
2024-05-09 08:54:26 +0000

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,45 @@
+#Defining a List
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#Accessing a List by Position 
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+#Changing the values in a list
+myFruitList[2] = "orange"
+print(myFruitList)
+
+#Defining a Tuple
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+#Accessing a tuple by position
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+#Definning a Dictionary
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+#Accessing a dictionary by name
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
+
+
+
+
+
+
+
---
+++ numeric-data.py
@@ -0,0 +1,30 @@
+print("Python has three numeric types: int, float, and complex")
+
+#String Data type
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Float Data type
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Complex Data type
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#Boolean Data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
